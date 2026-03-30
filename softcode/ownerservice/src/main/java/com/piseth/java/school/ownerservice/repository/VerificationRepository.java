5fab7207a1bef210744117c1990400a8ce325143
longvibol <vibollong@gmail.com>
2026-03-30 15:49:03 +0700

    finished

---
+++ softcode/ownerservice/src/main/java/com/piseth/java/school/ownerservice/repository/VerificationRepository.java
@@ -0,0 +1,20 @@
+package com.piseth.java.school.ownerservice.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+import com.piseth.java.school.ownerservice.domain.Verification;
+import com.piseth.java.school.ownerservice.domain.enums.VerificationType;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface VerificationRepository extends ReactiveCrudRepository<Verification, UUID> {
+
+	//Method query 
+	Mono<Verification> findFirstByOwnerIdAndTypeAndVerifiedFalseOrderByCreatedAtDesc(UUID ownerId,
+			VerificationType type);
+
+	Flux<Verification> findByOwnerIdAndTypeAndVerifiedFalse(UUID ownerId, VerificationType type);
+}
